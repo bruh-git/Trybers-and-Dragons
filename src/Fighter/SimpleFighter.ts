2a67d53190a726208c3372ba0dcd05fb2afd6fac
bruh-git <brunac.cardososilva@gmail.com>
2022-08-10 12:07:47 -0300

    Requisito 7 em andamento

---
+++ src/Fighter/SimpleFighter.ts
@@ -0,0 +1,5 @@
+import Fighter from '../Fighter';
+
+export default interface SimpleFighter extends Fighter{
+  attack(enemy: SimpleFighter): void;
+}
---
+++ src/Fighter/index.ts
@@ -0,0 +1,3 @@
+import SimpleFighter from './SimpleFighter';
+
+export default SimpleFighter;
