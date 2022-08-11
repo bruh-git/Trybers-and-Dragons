0c2ecd1d4589d79729e013383148cf22477e0b1f
bruh-git <brunac.cardososilva@gmail.com>
2022-08-10 21:00:01 -0300

    Requisito 13
    Em desenvolvimento
    
    Concluído.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
-import Battle, { PVE, PVP } from './Battle';
 import Character from './Character';
-import Dragon from './Dragon';
 import Monster from './Monster';
+import Dragon from './Dragon';
+import Battle, { PVE, PVP } from './Battle';
+import getRandomInt from './utils';
 
-const player1 = new Character('Player1');
-const player2 = new Character('Player2');
-const player3 = new Character('Player3');
+const player1 = new Character('Bruna');
+const player2 = new Character('Eduarda');
+const player3 = new Character('Thiago');
 
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
+for (let i = 0; i < getRandomInt(1, 5) + 1; i += 1) {
+  player1.levelUp();
+}
 
 const monster1 = new Monster();
 const monster2 = new Dragon();
 
 const pvp = new PVP(player2, player3);
-
 const pve = new PVE(player1, [monster1, monster2]);
 
 const runBattles = (battles: Battle[]) => {
-  battles.forEach((battle) => battle.fight());
+  battles.forEach((battle) => {
+    battle.fight();
+  });
 };
 
-export { runBattles, pve, pvp, player1, player2, player3, monster1, monster2 };
+export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
